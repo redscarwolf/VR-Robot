87ca6983b858857253562bc124e3f5cb02d4f4ac
DanielHipp <d.hipp@gmx.net>
2017-04-18 19:14:04 +0200

    added Animator + TestAsset + easy BoredScript

---
+++ VR-Robot-Default/Assets/Scripts/Bored.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Bored : MonoBehaviour {
+
+    Animator anim;
+
+    public float waittime;
+    public float resettime = 1.0f;
+
+    private float startTime;
+    private bool boredActivated;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        startTime = Time.time;
+        boredActivated = false;
+    }
+    
+    void Update()
+    {
+        float thisTime = Time.time;
+        //wie IDLE animation unterbrechen?
+        if (thisTime - startTime >= waittime)
+        {
+            startTime = thisTime;
+            boredActivated = true;
+        }
+        if(thisTime - startTime >= resettime)
+        {
+            boredActivated = false;
+        }
+        anim.SetBool("bored", boredActivated);
+        anim.SetFloat("time", thisTime);
+    }
+}
