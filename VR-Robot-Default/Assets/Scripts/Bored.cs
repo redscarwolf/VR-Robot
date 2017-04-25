74a1af0e73d51b2e9ab4e2626bcdbae2d4c0eaad
redscarwolf <kieser.ma@gmail.com>
2017-04-25 18:01:37 +0200

    manta blume wuerfel added

--- VR-Robot-Default/Assets/Scripts/Bored.cs
+++ VR-Robot-Default/Assets/Scripts/Bored.cs
@@ -2,26 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bored : MonoBehaviour {
+public class bored : StateMachineBehaviour {
 
-    Animator anim;
+	private float counter = 0.0f;
+	public int DELAY = 5;
 
-    public float waittime;
-    public float resettime = 1.0f;
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
 
-    private float startTime;
-    
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        startTime = Time.time;
-    }
-    
-    void Update()
-    {
-		
-    }
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 
+		counter += Time.deltaTime;
 
+		if (counter >= DELAY) {
+			//Debug.Log(anim.GetCurrentAnimatorStateInfo (1).ToString ());
+			animator.SetTrigger ("BOREDTRIGGER");
+			counter = 0;
+		}
+	}
 
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
 }
